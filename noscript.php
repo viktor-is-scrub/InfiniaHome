454834e7aacf7a3b55e78bc4f08a6c66b8931cac
xiurobert <xiurobert@users.noreply.github.com>
2016-07-01 15:29:17 +0800

    Make NoScript File

---
+++ noscript.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TERN ON YOUR JAVASCRIT</title>
+    </head>
+    <body>
+        <p>Please enable JavaScript to view InfiniaPress
+        </p>
+    </body>
+</html>
