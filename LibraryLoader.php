424c9bce8d435f7a55d17babf8b02d6ecbe95b5b
xiurobert <xiurobert@gmail.com>
2016-07-08 18:25:43 +0800

    Make a library Loader

---
+++ LibraryLoader.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * InfiniaPress Library Loader System
+ * Written by xiurobert
+ * This is an extremely hardcoded program and it is not advised you use it in your code
+ */
+
+
+    /**
+     * Function to make a Web Library e.g. Bootstrap, jQuery etc.
+     * @deprecated
+     * @param String $path Links to the path of the library, relative to the location of the current file
+     * @param String $alias Give the library an alias
+     */
+    $libraries = Array();
+    
+    function addWebLibrary($path, $alias) {
+        
+    }
+ 
