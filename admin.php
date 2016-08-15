27f2917c46b54f734d75d666d4f9d168be1f9b1d
xiurobert <xiurobert@users.noreply.github.com>
2016-08-15 20:39:40 +0800

    Add admin utility

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created in GitHub Web
+ * ------------------------------------
+ * InfiniaPress Administration Utility
+ * ------------------------------------
+ * (c) 2016 InfiniaPress Open Source Foundation
+ * TM & (R) (C) Keane Totally Wrote PHP
+ */
