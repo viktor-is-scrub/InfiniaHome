4247fb0a99888420b82ca5511ee2010d9b912dd2
Robert Xiu <xiurobert@gmail.com>
2016-09-12 20:20:53 +0800

    Redo Signup unit tests

--- test/SignupTest.php
+++ test/SignupTest.php
@@ -13,20 +13,20 @@ class SignupTest extends TestCase {
     public function testDatabaseConnection() {
         $test_details = json_decode(file_get_contents("../test_info.json"), true);
 
-        $dbconn = new mysqli($test_details['host'], $test_details['username'], $test_details['password'], $test_details['database'], $test_details['port']);
+        $dbconn = new mysqli($test_details['database']['host'], $test_details['database']['username'], $test_details['database']['password'], $test_details['database']['database_name'], $test_details['database']['port']);
         $test = new User($dbconn);
     }
 
     public function testSignup() {
         $test_details = json_decode(file_get_contents("../test_info.json"), true);
 
-        $dbconn = new mysqli($test_details['host'], $test_details['username'], $test_details['password'], $test_details['database'], $test_details['port']);
+        $dbconn = new mysqli($test_details['database']['host'], $test_details['database']['username'], $test_details['database']['password'], $test_details['database']['database_name'], $test_details['database']['port']);
         $test = new User($dbconn);
 
-        $fakeusername = $test['test_username'];
-        $fakepassword = $test['test_password'];
-        $fakeemail = $test['test_email'];
-        $fakename = $test['test_name'];
+        $fakeusername = $test_details['dummy_user']['username'];
+        $fakepassword = $test_details['dummy_user']['password'];
+        $fakeemail = $test_details['dummy_user']['email'];
+        $fakename = $test_details['dummy_user']['name'];
         $fakecode = hash("sha256", uniqid(rand()), true);
 
         $test->signup($fakeusername, $fakepassword, $fakeemail, $fakename, $fakecode);
@@ -37,9 +37,9 @@ class SignupTest extends TestCase {
     public function testEmailSend() {
         $test_details = json_decode(file_get_contents("../test_info.json"), true);
 
-        $dbconn = new mysqli($test_details['host'], $test_details['username'], $test_details['password'], $test_details['database'], $test_details['port']);
+        $dbconn = new mysqli($test_details['database']['host'], $test_details['database']['username'], $test_details['database']['password'], $test_details['database']['database_name'], $test_details['database']['port']);
         $test = new User($dbconn);
 
-        //test->send_mail();
+        //$test->send_mail();
     }
 }
